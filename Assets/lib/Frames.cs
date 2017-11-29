b39e40be8ef472af5694510f1a2aae013a226cac
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-11-29 00:10:54 -0800

    Supply a frame representing Unity

---
+++ Assets/lib/Frames.cs
@@ -0,0 +1,6 @@
+﻿namespace FrameConversions {
+    static class Frames {
+        static CoordinateFrame _Unity = null;
+        static CoordinateFrame Unity { get { return _Unity = _Unity ?? new CoordinateFrame("+X+Y-Z", "-Z-X-Y"); } }
+    }
+}
