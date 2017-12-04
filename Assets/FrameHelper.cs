2fe2719a9210afb3ab5c592ff409aecad7e3faa3
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-12-04 09:10:24 -0800

    More frame helper updates

--- Assets/FrameHelper.cs
+++ Assets/FrameHelper.cs
@@ -2,11 +2,16 @@
 using FrameConversions;
 
 public class FrameHelper : MonoBehaviour {
+    public bool AlwaysDraw = false;
+
+    [Header("Coordinate Frame")]
     public string Axes = "XY-Z";
     public string RotationOrder = "-Z-X-Y";
-
-    private void OnDrawGizmosSelected() {
+    void Draw() {
         Gizmos.matrix = transform.localToWorldMatrix;
         Utilities.DrawFrame(new AxisSet(Axes), new AxisSet(RotationOrder, false), 0.5f);
     }
+
+    private void OnDrawGizmosSelected() { if (!AlwaysDraw) Draw(); }
+    private void OnDrawGizmos() { if (AlwaysDraw) Draw(); }
 }
