fc805e32503328c22536cc06c531c385fe9ed962
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-12-04 11:33:26 -0800

    Add scale to the frame helper

--- Assets/FrameHelper.cs
+++ Assets/FrameHelper.cs
@@ -3,13 +3,14 @@ using FrameConversions;
 
 public class FrameHelper : MonoBehaviour {
     public bool AlwaysDraw = false;
+    public float Scale = 1;
 
     [Header("Coordinate Frame")]
     public string Axes = "XY-Z";
     public string RotationOrder = "-Z-X-Y";
     void Draw() {
         Gizmos.matrix = transform.localToWorldMatrix;
-        Utilities.DrawFrame(new AxisSet(Axes, false), new AxisSet(RotationOrder, true), 0.5f);
+        Utilities.DrawFrame(new AxisSet(Axes, false), new AxisSet(RotationOrder, true), Scale);
     }
 
     private void OnDrawGizmosSelected() { if (!AlwaysDraw) Draw(); }
