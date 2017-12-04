2bf72cd69b73460d4d80c10b8327782c6605d505
Garrett Johnson <garrett.kjohnson@gmail.com>
2017-12-04 15:48:33 -0800

    Make the pre-made frames public

--- Assets/lib/Frames.cs
+++ Assets/lib/Frames.cs
@@ -1,14 +1,14 @@
 ﻿namespace FrameConversions {
     static class Frames {
 
-        static CoordinateFrame _Unity = null;
-        static CoordinateFrame Unity { get { return _Unity = _Unity ?? new CoordinateFrame("+X+Y-Z", "-Z-X-Y"); } }
+        public static CoordinateFrame _Unity = null;
+        public static CoordinateFrame Unity { get { return _Unity = _Unity ?? new CoordinateFrame("+X+Y-Z", "-Z-X-Y"); } }
 
-        static CoordinateFrame _ROS = null;
-        static CoordinateFrame ROS { get { return _ROS = _ROS ?? new CoordinateFrame("-Y+Z+X", "+Z+Y+X"); } }
+        public static CoordinateFrame _ROS = null;
+        public static CoordinateFrame ROS { get { return _ROS = _ROS ?? new CoordinateFrame("-Y+Z+X", "+Z+Y+X"); } }
 
-        static CoordinateFrameConverter _Unity2ROS = null;
-        static CoordinateFrameConverter Unity2ROS { get { return _Unity2ROS = _Unity2ROS ?? new CoordinateFrameConverter(Unity, ROS); } }
-        static CoordinateFrameConverter ROS2Unity { get { return Unity2ROS.inverse; } }
+        public static CoordinateFrameConverter _Unity2ROS = null;
+        public static CoordinateFrameConverter Unity2ROS { get { return _Unity2ROS = _Unity2ROS ?? new CoordinateFrameConverter(Unity, ROS); } }
+        public static CoordinateFrameConverter ROS2Unity { get { return Unity2ROS.inverse; } }
     }
 }
